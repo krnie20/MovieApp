7457ba6dce726b9c766710109ad24d59cd7c8203
Tobias Krongaard Piekut <tobias@piekut.dk>
2022-04-29 13:15:44 +0200

    First draft working
    The View slider on top is working

--- app/app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val lstSlides: MutableList<Slide> = ArrayList()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Add more slides as necessary
+        lstSlides.add(Slide(R.drawable.slide_1, "Avengers"))
+        lstSlides.add(Slide(R.drawable.slide_2, "Ironman"))
+
+        val sliderpager: ViewPager = findViewById<ViewPager>(R.id.slider_page)
+        sliderpager.adapter = SliderPagerAdapter(this, lstSlides)
     }
 }
