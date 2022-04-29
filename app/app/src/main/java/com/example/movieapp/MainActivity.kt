92cd962c23de14a919358fa62ee3ac12c8ecb05a
Tobias Krongaard Piekut <tobias@piekut.dk>
2022-04-29 15:50:02 +0200

    Working slider indicator
    Does not have a timer implemented, but the app now works with view binding

--- app/app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.movieapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val lstSlides: MutableList<Slide> = ArrayList()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // Add more slides as necessary
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val indicator: TabLayout = binding.indicator // this.findViewById(R.id.indicator)
+        // Add more slides as necessary ---------------------------
         lstSlides.add(Slide(R.drawable.slide_1, "Avengers"))
         lstSlides.add(Slide(R.drawable.slide_2, "Ironman"))
 
-        val sliderpager: ViewPager = findViewById<ViewPager>(R.id.slider_page)
+        //---------------------------------------------------------
+
+        val sliderpager: ViewPager = binding.sliderPage //this.findViewById<ViewPager>(R.id.slider_page)
         sliderpager.adapter = SliderPagerAdapter(this, lstSlides)
+        indicator.setupWithViewPager(sliderpager, true)
+
+        // Setting the viewbinding
+        val view = binding.root
+        setContentView(view)
     }
 }
+
