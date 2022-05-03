d71d0798b8f27dab3f85a1d88dce883c30d2b526
Tobias Krongaard Piekut <tobias@piekut.dk>
2022-05-03 19:40:58 +0200

    Implemented the RecyclerView

--- app/app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.movieapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.movieapp.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val lstSlides: MutableList<Slide> = ArrayList()
+        val lstMovie: MutableList<Movie> = ArrayList()
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,9 +25,26 @@ class MainActivity : AppCompatActivity() {
 
         //---------------------------------------------------------
 
+        // Add more movies ----------------------------------------
+        lstMovie.add(Movie(R.drawable.slide_1, "Avengers"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        lstMovie.add(Movie(R.drawable.slide_2, "Ironman"))
+        //---------------------------------------------------------
+
         val sliderpager: ViewPager = binding.sliderPage //this.findViewById<ViewPager>(R.id.slider_page)
         sliderpager.adapter = SliderPagerAdapter(this, lstSlides)
         indicator.setupWithViewPager(sliderpager, true)
+
+        // Set up the movie adapter
+        val movieRV: RecyclerView = binding.RvMovies
+        movieRV.adapter = MovieAdapter(this, lstMovie)
+        movieRV.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
 
         // Setting the viewbinding
         val view = binding.root
---
+++ app/app/src/main/java/com/example/movieapp/MovieAdapter.java
@@ -0,0 +1,59 @@
+package com.example.movieapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+
+        Context context;
+        List<Movie> mData;
+
+    public MovieAdapter(Context context, List<Movie> mData) {
+        this.context = context;
+        this.mData = mData;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView TvTitle;
+        private ImageView ImgMovie;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            TvTitle = itemView.findViewById(R.id.movie_item_title);
+            ImgMovie = itemView.findViewById(R.id.movie_item_img);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.movie_item, parent, false);
+        return new MyViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.TvTitle.setText(mData.get(position).getTitle());
+        holder.ImgMovie.setImageResource(mData.get(position).getThumbnail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+}
