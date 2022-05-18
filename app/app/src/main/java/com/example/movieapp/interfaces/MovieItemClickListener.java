de8dfc77e3d99c686c6598ace97fc246d3619e8d
Tobias Krongaard Piekut <tobias@piekut.dk>
2022-05-18 13:31:02 +0200

    Size testing

--- app/app/src/main/java/com/example/movieapp/interfaces/MovieItemClickListener.java
+++ app/app/src/main/java/com/example/movieapp/interfaces/MovieItemClickListener.java
@@ -4,8 +4,5 @@ import android.widget.ImageView;
 import com.example.movieapp.model.Movie;
 
 public interface MovieItemClickListener {
-
     void onMovieClick(Movie movie, ImageView movieImageView); // we will need the imageview to make the shared animation.
-
-
 }
